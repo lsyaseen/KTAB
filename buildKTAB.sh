546bba951c3665cd7e332e77b78536617f2f40f0
Ben Wise <bwise@khobar.kapsarc.org>
2015-09-07 22:39:24 -0500

    add explanatory comment

--- buildKTAB.sh
+++ buildKTAB.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #
 # You must generate a 'build' directory for each subproject
+# and configure each one so it is buildable
 # before using this script. CMake is the recommended was to do so.
-#
+# This script just builds them all in order, as a shortcut
+# after you have set it up.
 #------------------------------------------
 FILES="kutils  kmodel  power-dispatch  refpri"
 
