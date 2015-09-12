2090ecd44a67edf21905ce3710a67645a44fc308
Ben Wise <bwise@khobar.kapsarc.org>
2015-09-11 21:15:46 -0500

    update the CMake helper files

--- cmakemodules/Findkmodel.cmake
+++ cmakemodules/Findkmodel.cmake
@@ -24,58 +24,58 @@
 # This is an example of how to build one for an uncommon library.
 # -------------------------------------------------
 
-set(KUTILS_FOUND "NO")
+set(KMODEL_FOUND "NO")
 
-set(KUTILS_PREFIX
+set(KMODEL_PREFIX
   "/usr/local/ktab/"
   CACHE STRING
-  "kutils is installed in this prefix (if non-standard)"
+  "kmodel is installed in this prefix (if non-standard)"
   )
 
   # Note Well the locations in and order of search
-set(KUTILS_POSSIBLE_PATHS
-  ${KUTILS_PREFIX}/include/
-  ${KUTILS_PREFIX}/lib/
-  ${CMAKE_CURRENT_SOURCE_DIR}/../kutils/
-  ${CMAKE_CURRENT_SOURCE_DIR}/../build/kutils/
-  ${CMAKE_CURRENT_SOURCE_DIR}/../../kutils/
-  ${CMAKE_CURRENT_SOURCE_DIR}/../../KTAB/kutils/
+set(KMODEL_POSSIBLE_PATHS
+  ${KMODEL_PREFIX}/include/
+  ${KMODEL_PREFIX}/lib/
+  ${CMAKE_CURRENT_SOURCE_DIR}/../kmodel/
+  ${CMAKE_CURRENT_SOURCE_DIR}/../build/kmodel/
+  ${CMAKE_CURRENT_SOURCE_DIR}/../../kmodel/
+  ${CMAKE_CURRENT_SOURCE_DIR}/../../KTAB/kmodel/
   ${KTAB_PREFIX}
   "C:/local/ktab"
-  ${CMAKE_CURRENT_SOURCE_DIR}/../kutils/Debug
-  ${CMAKE_CURRENT_SOURCE_DIR}/../kutils/Release
+  ${CMAKE_CURRENT_SOURCE_DIR}/../kmodel/Debug
+  ${CMAKE_CURRENT_SOURCE_DIR}/../kmodel/Release
   )
 
 # try to find a key header
-find_path(KUTILS_INCLUDE_DIR kutils.h
+find_path(KMODEL_INCLUDE_DIR kmodel.h
   PATH_SUFFIXES libsrc
-  PATHS ${KUTILS_POSSIBLE_PATHS}
+  PATHS ${KMODEL_POSSIBLE_PATHS}
   )
 
 # try to find the compiled library object
-find_library(KUTILS_LIBRARY NAMES kutils
-  PATHS ${KUTILS_POSSIBLE_PATHS}
+find_library(KMODEL_LIBRARY NAMES kmodel
+  PATHS ${KMODEL_POSSIBLE_PATHS}
   )
 
 # if the library object was found, record it.
 # may need to add other system files latter.
-if(KUTILS_LIBRARY)
-  set(KUTILS_LIBRARIES  ${KUTILS_LIBRARY})
-endif(KUTILS_LIBRARY)
+if(KMODEL_LIBRARY)
+  set(KMODEL_LIBRARIES  ${KMODEL_LIBRARY})
+endif(KMODEL_LIBRARY)
 
 # if found both library object and that key header,
 # record the data and let the user know it succeeded
-if(KUTILS_LIBRARIES AND KUTILS_INCLUDE_DIR)
-  set(KUTILS_FOUND "YES")
-  message(STATUS "Found kutils library and headers: ")
-  message(STATUS "  ${KUTILS_LIBRARY}")
-  message(STATUS "  ${KUTILS_INCLUDE_DIR}")
-endif(KUTILS_LIBRARIES AND KUTILS_INCLUDE_DIR)
+if(KMODEL_LIBRARIES AND KMODEL_INCLUDE_DIR)
+  set(KMODEL_FOUND "YES")
+  message(STATUS "Found kmodel library and headers: ")
+  message(STATUS "  ${KMODEL_LIBRARY}")
+  message(STATUS "  ${KMODEL_INCLUDE_DIR}")
+endif(KMODEL_LIBRARIES AND KMODEL_INCLUDE_DIR)
 
 # if not found, stop immediately
-if(NOT KUTILS_FOUND)
-  message(FATAL_ERROR "Could not find kutils library and headers")
-endif(NOT KUTILS_FOUND)		
+if(NOT KMODEL_FOUND)
+  message(FATAL_ERROR "Could not find kmodel library and headers")
+endif(NOT KMODEL_FOUND)		
 		
 # --------------------------------------------
 # Copyright KAPSARC. Open source MIT License.
