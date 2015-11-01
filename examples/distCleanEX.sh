cb3d1812e7fb9823c7d0381cd6fcc2abfb912253
Ben Wise <bwise@khobar.kapsarc.org>
2015-11-01 10:39:49 +0300

    fix up SMP example to use KGraph

---
+++ examples/distCleanEX.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This wipes out all the "build" directories, so you
+# will have to run CMake individually on each to
+# rebuild them
+#
+#------------------------------------------
+FILES="minwater  reformpri  smp"
+
+for d in $FILES
+do
+pushd $d/build; make clean; popd
+pushd $d; rm -rf build ; mkdir build; sleep 1; popd
+done
+#------------------------------------------
