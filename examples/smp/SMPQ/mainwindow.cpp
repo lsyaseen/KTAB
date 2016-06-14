aef7755f5b8d0562400abe0d0cd32cdd511cca35
arunsathvik <arunsathvik.n@gmail.com>
2016-06-14 09:27:31 +0530

    Update SMPQ  GUI

--- examples/smp/SMPQ/mainwindow.cpp
+++ examples/smp/SMPQ/mainwindow.cpp
@@ -1,83 +1,203 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow()
 {
-    ui->setupUi(this);
+    central = new QFrame;
+    QGridLayout *gLayout = new QGridLayout(central);
 
-    //action for read from file
-    connect(ui->actionRead_from_file,SIGNAL(triggered(bool)),this,SLOT(csv_read_Action(bool)));
-    model = new QStandardItemModel;
+    csv_tableView = new QTableView(central);
+    gLayout->addWidget(csv_tableView);
+    central->setLayout(gLayout);
+
+    setCentralWidget(central);
+
+    central->setFrameShape(QFrame::StyledPanel);
+
+    createActions();
+    createStatusBar();
+
+    createModuleParametersDockWindow();
+    createGraph1DockWindows();
+    createGraph2DockWindows();
+
+    setWindowTitle(tr("SMPQ_Dock_GUI"));
+
+    //CSV class obj
+    csvObj = new CSV;
+    //connect statement to pass the csv file path
+    connect(this,&MainWindow::csvFilePath, csvObj, &CSV::readCSVFile);
+    //connect statement to receive processed model
+    connect(csvObj, &CSV::csvModel,this,&MainWindow::csvItemModel);
 
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
 }
 
-void MainWindow::csv_read_Action(bool action)
+
+void MainWindow::csvGetFilePAth()
 {
-
-    QStringList tableHeaders;
-    //getting the csv file path
+    //Get  *.csv file path
     QString FilePath;
-    FilePath = QFileDialog::getOpenFileName(this,tr("Open CSV File"), QDir::homePath() , tr("File Description (*.csv)"));
+    FilePath = QFileDialog::getOpenFileName(this,tr("Open CSV File"), QDir::homePath() , tr("CSV File (*.csv)"));
+    statusBar()->showMessage(tr("csv path"));
 
-    if(!FilePath.isEmpty())
-    {
-        model->clear();
-
-        QFile file(FilePath);
-        if (file.open(QIODevice::ReadOnly))
-        {
-            int rowindex = 0;                     // file row counter
-            QTextStream in(&file);                 // read to text stream
-
-            while (!in.atEnd())
-            {
-                // read one line from textstream(separated by "\n")
-                QString fileLine = in.readLine();
-
-                // parse the read line into separate pieces(tokens) with "," as the delimiter
-                QStringList lineToken = fileLine.split(",", QString::SkipEmptyParts);
-
-                if(rowindex >= 2 )
-                {
-                    // load parsed data to model accordingly
-                    for (int j = 0; j < lineToken.size(); j++)
-                    {
-                        QString value = lineToken.at(j);
-                        QStandardItem *item = new QStandardItem(value.trimmed());
-                        model->setItem(rowindex-2, j, item);
-                    }
-                }
-                else if(rowindex == 1)
-                {
-                    model->setHorizontalHeaderLabels(lineToken);
-
-                }
-                else
-                {
-                    lineToken.clear();
-                }
-                rowindex++;
-            }
-
-            file.close();
-        }
-        ui->csv_tableView->setModel(model);
-        ui->csv_tableView->showMaximized();
-        ui->csv_tableView->setAlternatingRowColors(true);
-        ui->csv_tableView->resizeRowsToContents();
-
-        for (int c = 0; c < ui->csv_tableView->horizontalHeader()->count(); ++c)
-            ui->csv_tableView->horizontalHeader()->setSectionResizeMode(c, QHeaderView::Stretch);
-
-        ui->csv_tableView->resizeColumnsToContents();
-
-    }
+    //emit path to csv class for processing
+    emit csvFilePath(FilePath);
 }
 
+void MainWindow::dockWindowChanged()
+{
+    QWidget *TitleWidgetRec=new QWidget(this);
+    if(graph2Dock->isFloating())
+        graph2Dock->setTitleBarWidget(TitleWidgetRec);
+    else
+        graph2Dock->setTitleBarWidget(0);
+}
+
+void MainWindow::csvItemModel(QStandardItemModel *model)
+{
+    //update received model to widget
+    csv_tableView->setModel(model);
+    csv_tableView->showMaximized();
+    csv_tableView->setAlternatingRowColors(true);
+    csv_tableView->resizeRowsToContents();
+
+    //    for (int c = 0; c < csv_tableView->horizontalHeader()->count(); ++c)
+    //        csv_tableView->horizontalHeader()->setSectionResizeMode(c, QHeaderView::Stretch);
+
+    csv_tableView->resizeColumnsToContents();
+
+    //Enable run button, which is disabled by default
+    runButton->setEnabled(true);
+}
+
+void MainWindow::about()
+{
+    QMessageBox::about(this, tr("About SMP "),
+                       tr("Should update informtion regarding SMP here !! "));
+}
+
+void MainWindow::createActions()
+{
+    QMenu *fileMenu = menuBar()->addMenu(tr("&File"));
+    QToolBar *fileToolBar = addToolBar(tr("File"));
+
+    const QIcon newIcon = QIcon::fromTheme("document-new", QIcon(":/images/new.png"));
+    QAction *newDoc = new QAction(newIcon, tr("&New "), this);
+    newDoc->setShortcuts(QKeySequence::New);
+    newDoc->setStatusTip(tr("Create new "));
+    fileMenu->addAction(newDoc);
+    fileToolBar->addAction(newDoc);
+
+    const QIcon saveIcon = QIcon::fromTheme("document-save", QIcon(":/images/save.png"));
+    QAction *saveAct = new QAction(saveIcon, tr("&Save..."), this);
+    saveAct->setShortcuts(QKeySequence::Save);
+    saveAct->setStatusTip(tr("Save"));
+    fileMenu->addAction(saveAct);
+    fileToolBar->addAction(saveAct);
+
+    const QIcon csvIcon = QIcon::fromTheme("read-csv", QIcon(":/images/csv.png"));
+    QAction *readCsvAct = new QAction(csvIcon, tr("&Read CSV"), this);
+    readCsvAct->setShortcuts(QKeySequence::Save);
+    readCsvAct->setStatusTip(tr("Save the current form letter"));
+    connect(readCsvAct, &QAction::triggered, this, &MainWindow::csvGetFilePAth);
+    fileMenu->addAction(readCsvAct);
+    fileToolBar->addAction(readCsvAct);
+
+    fileMenu->addSeparator();
+
+    QAction *quitAct = fileMenu->addAction(tr("&Quit"), this, &QWidget::close);
+    quitAct->setShortcuts(QKeySequence::Quit);
+    quitAct->setStatusTip(tr("Quit the application"));
+
+
+    viewMenu = menuBar()->addMenu(tr("&View"));
+
+    menuBar()->addSeparator();
+
+    QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
+
+    QAction *aboutAct = helpMenu->addAction(tr("&About"), this, &MainWindow::about);
+    aboutAct->setStatusTip(tr("Show the application's About box"));
+
+}
+
+void MainWindow::createStatusBar()
+{
+    statusBar()->showMessage(tr("Ready"));
+}
+
+void MainWindow::createGraph1DockWindows()
+{
+    graph1Dock = new QDockWidget(tr("Module 1"), this);
+    //   graph1Dock->setAllowedAreas(Qt::LeftDockWidgetArea |Qt::RightDockWidgetArea |Qt::BottomDockWidgetArea | Qt::TopDockWidgetArea);
+    listwidget = new QListWidget(graph1Dock);
+    graph1Dock->setWidget(listwidget);
+    addDockWidget(Qt::BottomDockWidgetArea, graph1Dock);
+    viewMenu->addAction(graph1Dock->toggleViewAction());
+
+    connect(graph1Dock,SIGNAL(dockLocationChanged(Qt::DockWidgetArea)),this,SLOT(dockWindowChanged()));
+}
+
+void MainWindow::createGraph2DockWindows()
+{
+    graph2Dock = new QDockWidget(tr("Module2"), this);
+    //  graph2Dock->setAllowedAreas(Qt::LeftDockWidgetArea |Qt::RightDockWidgetArea |Qt::BottomDockWidgetArea | Qt::TopDockWidgetArea);
+    listwidget = new QListWidget(graph2Dock);
+    graph2Dock->setWidget(listwidget);
+    addDockWidget(Qt::BottomDockWidgetArea, graph2Dock);
+    viewMenu->addAction(graph2Dock->toggleViewAction());
+
+    connect(graph2Dock,SIGNAL(dockLocationChanged(Qt::DockWidgetArea)),this,SLOT(dockWindowChanged()));
+
+}
+
+void MainWindow::createModuleParametersDockWindow()
+{
+    moduleParametersDock = new QDockWidget(tr(" Parameters"),this);
+
+    moduleFrame = new QFrame(moduleParametersDock);
+    VLayout  = new QGridLayout(moduleFrame);
+
+    for (int i = 0; i < N; ++ i)
+    {
+        frames[i] = new QFrame(moduleParametersDock);
+        frames[i]->setFrameShape(QFrame::StyledPanel);//change to flat if required
+        VLayout->addWidget(frames[i]);
+    }
+
+    //RUN Button
+    runButton = new QPushButton;
+    runButton->setText("RUN");
+    runButton->setEnabled(false);
+    VLayout->addWidget(runButton);
+
+    //Radio buttons //Section1
+    QGridLayout *radioButtonLayout = new QGridLayout(frames[0]);
+    rparam1 = new QRadioButton("Parameter 1");
+    rparam2 = new QRadioButton("Parameter 2");
+    rparam3 = new QRadioButton("Parameter 3");
+    radioButtonLayout->addWidget(rparam1);
+    radioButtonLayout->addWidget(rparam2);
+    radioButtonLayout->addWidget(rparam3);
+    frames[0]->setLayout(radioButtonLayout);
+
+    //CheckBoxes //Section2
+    QGridLayout * checkBoxLayout = new QGridLayout(frames[1]);
+    cparam1 = new QCheckBox("Parameter 1");
+    cparam2 = new QCheckBox("Parameter 2");
+    cparam3 = new QCheckBox("Parameter 3");
+    checkBoxLayout->addWidget(cparam1);
+    checkBoxLayout->addWidget(cparam2);
+    checkBoxLayout->addWidget(cparam3);
+    frames[1]->setLayout(checkBoxLayout);
+
+    moduleParametersDock->setWidget(moduleFrame);
+    // moduleParametersDock->setAllowedAreas(Qt::RightDockWidgetArea | Qt::LeftDockWidgetArea | Qt::TopDockWidgetArea | Qt::BottomDockWidgetArea );
+    addDockWidget(Qt::LeftDockWidgetArea, moduleParametersDock);
+    viewMenu->addAction(moduleParametersDock->toggleViewAction());
+
+}
--- examples/smp/SMPQ/mainwindow.h
+++ examples/smp/SMPQ/mainwindow.h
@@ -2,28 +2,91 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "csv.h"
+
+//! [0]
+#include <QtWidgets>
+#ifndef QT_NO_PRINTDIALOG
+//#include <QtPrintSupport>
+#endif
+
 #include <QDebug>
-#include <QStandardItemModel>
-#include <QFile>
-#include <QFileDialog>
+class QAction;
+class QListWidget;
+class QMenu;
+class QTextEdit;
+
 
 namespace Ui {
 class MainWindow;
 }
 
+static const int N = 2;
+
 class MainWindow : public QMainWindow
 {
+
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    //explicit MainWindow(QWidget *parent = 0);
+    MainWindow();
     ~MainWindow();
 
 private slots:
-    void csv_read_Action(bool action);
+    void about();
+
+    void csvItemModel(QStandardItemModel * model);
+    void csvGetFilePAth();
+    void dockWindowChanged();
+ signals:
+
+    void csvFilePath(QString path);
 private:
-    Ui::MainWindow *ui;
-    QStandardItemModel * model;
+    void createActions();
+    void createStatusBar();
+    void createGraph1DockWindows();
+    void createGraph2DockWindows();
+    void createModuleParametersDockWindow();
+
+    //Model parameters
+    QFrame *central;
+
+    QFrame * frames[N];
+    QPushButton * runButton;
+
+
+    QRadioButton * rparam1;
+    QRadioButton * rparam2;
+    QRadioButton * rparam3;
+
+    QCheckBox * cparam1;
+    QCheckBox * cparam2;
+    QCheckBox * cparam3;
+
+    //csv
+    QTableView * csv_tableView;
+    CSV *csvObj;
+
+    QListWidget *listwidget;
+    QListWidget *listwidget2;
+
+    QMenu *viewMenu;
+
+
+    QDockWidget * moduleParametersDock;
+    QDockWidget * graph1Dock;
+    QDockWidget * graph2Dock;
+
+    QGridLayout * VLayout;
+    QFrame * moduleFrame;
+
+
+
+
+
+    //private:
+    //  Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
