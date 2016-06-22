844f9f76693e37d9439c612819983c6c4437b6b1
Ben Wise <bwise-ks16r@hotmail.com>
2016-06-22 15:15:44 +0300

    fix copyright notices to MIT; start quadratic map

--- KTAB/kgraph/src/qmmain.h
+++ KTAB/kgraph/src/qmmain.h
@@ -20,68 +20,44 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // --------------------------------------------
-// start of a simple Tetris clone
+// start of a quadratic map
+//---------------------------------------------
+#ifndef QUADMAP_MAIN_H
+#define QUADMAP_MAIN_H
+
+#include "qmutils.h"
 //---------------------------------------------
 
-#include <string.h>
-#include "tutils.h"
-#include "pvcanvas.h"
-#include "tmain.h"
+using std::cout;
+using std::endl;
+using std::flush;
 
+using KBase::PRNG;
+using KGraph::Canvas;
 
-namespace Tetris {
+//---------------------------------------------
+namespace QuadMap { 
 
-  PVCanvas::PVCanvas(int x, int y, int w, int h, const char * l) : Canvas(x, y, w, h, l) {
-    // nothing, yet
-  }
-
-
-  PVCanvas::~PVCanvas() {
-    // nothing, yet
-  }
-
-  void PVCanvas::onMove(int x, int y) {
-    return;
-  }
-  void PVCanvas::onDrag(int x, int y) {
-    return;
-  }
-  void PVCanvas::onPush(int x, int y, int b) {
-    return;
-  }
-  void PVCanvas::onRelease(int x, int y, int b) {
-    return;
-  }
-  void PVCanvas::onKeyDown(int x, int y, int k) {
-    return;
-  }
-  void PVCanvas::draw() {
-    const int px = x();
-    const int py = y();
-    const int pw = w();
-    const int ph = h();
-    clearMaps();
-    xMap = new KGraph::CoordMap(px, 0.5, px+pw, 5.5);
-    yMap = new KGraph::CoordMap(py + ph, 0.5, py, 3.5);
-    Canvas::draw();
-
-    fl_push_clip(px, py, pw, ph); 
-    Shape ns = TApp::theApp->board->nextShape;
-    TCode nc = ns.getShape();
-    const Fl_Color clr = TApp::theApp->color((unsigned int)nc);
-    const int ci = 2;
-    const int cj = 3;
-    for (int k = 0; k < 4; k++) {
-      TApp::theApp->board->drawUnitSquare(clr,
-        ci + ns.y(k), cj + ns.x(k),
-        false, FL_WHITE, this);
-    } 
-    fl_pop_clip();
-    return;
-  }
+    
+class QMApp {
+public:
+    explicit QMApp(uint64_t s);
+    virtual ~QMApp();
+    void run();
+    void quit();
+    PRNG* rng = nullptr;    
+    static QMApp* theApp;
+    
+protected:
+  
+private:
+    
+};
 
 }; // end of namespace
 
+//---------------------------------------------
+#endif
 // --------------------------------------------
 // Copyright KAPSARC. Open source MIT License.
 // --------------------------------------------
