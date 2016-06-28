3ea7500f7a926436bd9131367292775b4b349420
Ben Wise <bwise-ks16r@hotmail.com>
2016-06-28 08:08:04 +0300

    add a monte-carlo test script, with directory. modify random scenario ranges.

---
+++ examples/smp/mc-smpc-runs/monte-carlo-runs.sh
@@ -0,0 +1,36 @@
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Copyright KAPSARC. MIT Open Source License.
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# see http://zxq9.com/archives/795 for some date formats
+#!/bin/bash
+
+# 25 runs
+START=1000
+END=1024
+
+rm -f run-smpc*txt  run-smpc*db  test.db
+for i in $(eval echo "{$START..$END}")
+do
+  # Zulu (UTC) time, with nanoseconds
+  timestamp="$(date --utc +%Y-%m-%d_%H-%M-%S-%NZ)"
+  name="run-smpc-$timestamp"
+  echo "$i: $name" 
+  #../smpc --seed 0 --euSMP > $name.txt ; mv test.db $name.db
+done
+
+
+echo "Problem sizes"
+grep "Number of actors" run-smpc*.txt
+grep "Number of SMP dimensions" run-smpc*.txt
+
+
+sNum=$(grep "Start time" run-smpc*.txt | wc -l)
+cNum=$(grep "Elapsed time" run-smpc*.txt | wc -l)
+
+echo "Number of runs started   $sNum"
+echo "Number of runs completed $cNum"
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# Copyright KAPSARC. MIT Open Source License.
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
