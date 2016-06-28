bf97fe44d12c3c7c0640ee3d47d914907433621a
Ben Wise <bwise-ks16r@hotmail.com>
2016-06-28 08:11:11 +0300

    un-comment the monte-carlo command

--- examples/smp/mc-smpc-runs/monte-carlo-runs.sh
+++ examples/smp/mc-smpc-runs/monte-carlo-runs.sh
@@ -15,7 +15,7 @@ do
   timestamp="$(date --utc +%Y-%m-%d_%H-%M-%S-%NZ)"
   name="run-smpc-$timestamp"
   echo "$i: $name" 
-  #../smpc --seed 0 --euSMP > $name.txt ; mv test.db $name.db
+  nice ../smpc --seed 0 --euSMP > $name.txt ; mv test.db $name.db
 done
 
 
