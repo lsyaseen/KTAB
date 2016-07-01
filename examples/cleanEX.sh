10589bc42debbab3a18160f08ea91b7ce988eaca
Ben Wise <bwise.6bx1@gmail.com>
2016-07-01 19:15:37 +0300

    improve clean/build order of examples

--- examples/cleanEX.sh
+++ examples/cleanEX.sh
@@ -1,12 +1,13 @@
+#------------------------------------------
 #!/bin/bash
-#
+#------------------------------------------
 # You must generate a 'build' directory for each subproject
 # before using this script. CMake is the recommended was to do so.
 #
 #------------------------------------------
-FILES="minwater  reformpri  smp  agenda  comsel"
+DIRS="minwater  reformpri  agenda  smp  comsel"
 
-for d in $FILES
+for d in $DIRS
 do
 pushd $d/build ; nice make clean ; sleep 1; popd
 done
