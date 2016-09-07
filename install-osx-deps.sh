d8b4f287dd006e1d2f0d6382b5b1bbbb550abea4
David R. Pugh <david.pugh@kapsarc.org>
2016-09-07 09:08:51 +0300

    Refactor travis build matrix (#55)
    * First attempt at specifying a build matrix...
    
    * Fixed issues with build matrix
    
    * Changed the timing of tinxml2 install
    
    * Think I need to stop remove gcc and g++
    
    * See if the ordering matters...
    
    * Definitely don't remove g++ and gcc
    
    * Can I move before_script inside build matrix?
    
    * Fixed typo in symlink
    
    * Hopefully fixed build order!
    
    * Trying to find fltk library!
    
    * Now want to add OS specific install scripts
    
    * Need to ask permission!
    
    * Create install-linux-deps.sh script (#53)
    
    * I think this should fix permissions issues
    
    * Better to not use sudo within script!
    
    * Added OSX install script; fixed issue with sudo?
    
    * Added bash to see if that will get scripts to run.
    
    * Added support for clang-3.6; see if sudo is necessary.
    
    * Don't use sudo with brew.|
    
    * Added installation of fltk dependency.
    
    * Added symlink to clang-3.6 build.
    
    * Don't need to unlink non-existing symbolic link!
    
    * Improved docs.
    
    * Fixed symlic for clang?
    
    * Attempt to get clang 3.6 working
    
    * Added additional sources for clang.
    
    * Perhaps need to update g++ in order to get clang to work?
    
    * Fixed typo in clang-3.5 build.
    
    * Simplifying clang-3.5 build.

---
+++ install-osx-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install updated version of cmake
+brew install cmake
+cmake --version  # This allows us to confirm which version of cmake has been installed.
+
+# install ui dependencies
+brew install fltk
