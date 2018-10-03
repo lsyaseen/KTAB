db86a1095d130f3c2ce2723f1f96869a39ae5fef
Lama Yaseen <lama.yaseen@kapsarc.org>
2018-10-03 11:31:11 +0300

    Allow Unauthenticated for clang packages

--- install-linux-deps.sh
+++ install-linux-deps.sh
@@ -6,3 +6,5 @@ add-apt-repository -y ppa:george-edison55/cmake-3.x
 apt-get update --assume-yes
 apt-get install --assume-yes cmake
 cmake --version  # This allows us to confirm which version of cmake has been installed.
+
+apt-get --allow-unauthenticated upgrade
