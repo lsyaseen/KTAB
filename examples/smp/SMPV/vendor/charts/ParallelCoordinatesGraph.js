17ad400e3a709917ed0833c2e69f4c8c80edb809
linahhamdan <linah.hamdan@kapsarc.org>
2019-01-22 12:27:40 +0300

    Added 3 Tabs in Charts Page

---
+++ examples/smp/SMPV/vendor/charts/ParallelCoordinatesGraph.js
@@ -0,0 +1,14 @@
+function DrawScatterPlot() {
+    var allactorspos = arrPos;
+    console.log('allactorspos');
+    console.log(allactorspos);
+    }
+    
+    this.init = function () {
+    
+        DrawScatterPlot();
+    
+    }
+    
+    
+    this.init();
